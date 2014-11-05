e793b5dc1e3a09ef012f5d7983596978ddea3052
Elvio Martinelli <elviomartinelli@gmail.com>
2014-11-04 16:30:10 -0800

    New random

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,5 @@
-import System.Random
-import Data.Map
+module Main where
 
-fisherYatesStep :: RandomGen g => (Map Int a, g) -> (Int, a) -> (Map Int a, g)
-fisherYatesStep (m, gen) (i, x) = ((insert j x . insert i (m ! j)) m, gen')
-  where
-    (j, gen') = randomR (0, i) gen
- 
-fisherYates :: RandomGen g => g -> [a] -> ([a], g)
-fisherYates gen [] = ([], gen)
-fisherYates gen l = 
-  toElems $ Data.Map.foldl fisherYatesStep (initial (head l) gen) (numerate (tail l))
-  where
-    toElems (x, y) = (elems x, y)
-    numerate = zip [1..]
-    initial x gen = (singleton 0 x, gen)
+    random = [[w, x, y, z] | w <- [1..7], x <- [1..7], y <- [1..7], z <- [1..7], w /= x, w /= y, w /= z, x /= y, x /= z, y /= z]
+    main = print(random)
+
