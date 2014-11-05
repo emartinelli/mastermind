4ab8a666272ea7da211764d96ea9ff982f20ff83
Elvio Martinelli <elviomartinelli@gmail.com>
2014-11-04 16:06:37 -0800

    Fisher-Yattes shuffle

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,16 @@
-gerar n = gen 4
-    where gen 0 = [[]]
-          gen k = [a: x | a <- [1..7], x <- gen (k-1)]
+import System.Random
+import Data.Map
 
-
-main = print(gerar 2)
-
+fisherYatesStep :: RandomGen g => (Map Int a, g) -> (Int, a) -> (Map Int a, g)
+fisherYatesStep (m, gen) (i, x) = ((insert j x . insert i (m ! j)) m, gen')
+  where
+    (j, gen') = randomR (0, i) gen
+ 
+fisherYates :: RandomGen g => g -> [a] -> ([a], g)
+fisherYates gen [] = ([], gen)
+fisherYates gen l = 
+  toElems $ Data.Map.foldl fisherYatesStep (initial (head l) gen) (numerate (tail l))
+  where
+    toElems (x, y) = (elems x, y)
+    numerate = zip [1..]
+    initial x gen = (singleton 0 x, gen)
