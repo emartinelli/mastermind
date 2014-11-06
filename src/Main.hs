98e08855dc13a532c664bcc953eebdf6c5addbf2
Elvio Martinelli <elviomartinelli@gmail.com>
2014-11-06 13:48:31 -0800

    Random number

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,7 @@ module Main where
     gen_lst = [[w, x, y, z] | w <- [1..7], x <- [1..7], y <- [1..7], z <- [1..7], w /= x, w /= y, w /= z, x /= y, x /= z, y /= z]
     len_lst = length gen_lst
 
-    randomNumber = random (mkStdGen 100) :: (Int, StdGe)
+    randomNumber = random (mkStdGen 100) :: (Int, StdGen)
     list = gen_lst !! (fst randomNumber)
     main = print(list)
 
