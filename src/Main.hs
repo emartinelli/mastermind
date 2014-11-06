5a00fb18c4067ef761456b6446da22639d3b0728
Elvio Martinelli <elviomartinelli@gmail.com>
2014-11-06 13:47:45 -0800

    Random list

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,10 @@
 module Main where
+    import System.Random
 
-    random = [[w, x, y, z] | w <- [1..7], x <- [1..7], y <- [1..7], z <- [1..7], w /= x, w /= y, w /= z, x /= y, x /= z, y /= z]
-    main = print(random)
+    gen_lst = [[w, x, y, z] | w <- [1..7], x <- [1..7], y <- [1..7], z <- [1..7], w /= x, w /= y, w /= z, x /= y, x /= z, y /= z]
+    len_lst = length gen_lst
+
+    randomNumber = random (mkStdGen 100) :: (Int, StdGe)
+    list = gen_lst !! (fst randomNumber)
+    main = print(list)
 
