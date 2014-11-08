0add2153707299b65e7d80095441df2c857a73cc
Elvio Martinelli <elviomartinelli@gmail.com>
2014-11-08 12:43:10 -0800

    More UI. (Now with lasers, actually not yet :/)

--- src/Main.hs
+++ src/Main.hs
@@ -11,22 +11,32 @@ module Main where
                   [(x,"")]    -> Just x
                   _           -> Nothing
 
-    decreaseTries tries = tries-1
-
     getListFromString :: String -> Maybe [Integer]
     getListFromString str = maybeRead $ "[" ++ str ++ "]"
 
-    initialPrint :: String
-    initialPrint = "Let's play a game!"
+    welcome :: String
+    welcome = "Let's play a game!"
 
-    inMsg :: String
-    inMsg = "Enter with a sequence of 4 distinct numbers form 1 to 7 (separated by comma). Let's try!"
+    instructions :: String
+    instructions = "Enter with a sequence of 4 distinct numbers form 1 to 7 (separated by comma). Let's try!"
 
-    main = do
-        putStrLn initialPrint
-        putStrLn inMsg
+    formatError :: String
+    formatError = "Bad format. Good Bye."
+
+    game :: IO ()
+    game = do
+        gameInput
+
+    gameInput :: IO ()
+    gameInput = do
         input <- getLine
         let maybeList = getListFromString input in
             case maybeList of
-            Just l  -> print (sum l)
-            Nothing -> error "Bad format. Good Bye."
+                Just l  -> print (sum l)
+                Nothing -> error formatError
+
+    main :: IO ()
+    main = do
+        putStrLn welcome
+        putStrLn instructions
+        game
